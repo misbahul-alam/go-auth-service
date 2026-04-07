f26951e7d0069cf5fb44398acda33c24b5e45083
Misbahul Alam <misbahulalam64@gmail.com>
2026-04-07 20:53:20 +0600

    docs: initialize swagger api documentation

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,13 +2,26 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	_ "github.com/misbahul-alam/go-auth-service/docs"
 	"github.com/misbahul-alam/go-auth-service/internal/config"
 	"github.com/misbahul-alam/go-auth-service/internal/database"
 	"github.com/misbahul-alam/go-auth-service/internal/model"
 	"github.com/misbahul-alam/go-auth-service/internal/routes"
 	"github.com/misbahul-alam/go-auth-service/internal/validator"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
+// @title Go Auth Service
+// @version 1.0
+// @description A simple authentication backend built with Golang, Gin, and GORM. This project demonstrates core backend concepts like JWT authentication, password hashing, and clean structure.
+// @securityDefinitions.apikey BearerAuth
+// @in                         header
+// @name                       Authorization
+// @description                Type 'Bearer ' followed by your JWT token.
+
+// @host localhost:8080
+// @BasePath /api/v1
 func main() {
 
 	config.LoadEnv()
@@ -19,6 +32,7 @@ func main() {
 	r := gin.Default()
 
 	routes.RegisterRoutes(r)
+	r.GET("/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	err := r.Run(":8080")
 
